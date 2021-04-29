2c59fb7a63e43a5fef0aeefa3ce4c2b8e6963363
João Santos <joaopfs@outlook.com.br>
2021-04-29 03:25:48 -0300

    Update package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,19 @@ import styles from "../styles/app.module.scss";
 
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
-import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PlayerContext.Provider value={'Diego'}>
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-    </PlayerContext.Provider>
+    <PlayerContextProvider>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
   );
 }
 
